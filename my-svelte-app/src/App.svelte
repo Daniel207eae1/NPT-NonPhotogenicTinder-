70646409197015fdf8b4f033793fe4df19e03fb9
Daniel207eae1 <elbotcrack@gmail.com>
2023-02-24 21:52:56 -0500

    Commit3
    Perfil front 30%

--- my-svelte-app/src/App.svelte
+++ my-svelte-app/src/App.svelte
@@ -7,55 +7,60 @@
     bio: 'Hola xd'
   };
   let items = [
-    { title: "Inicio", link: "/", imageurl: "src\assets\Images\Home-removebg-preview.png"},
-    { title: "Notificaciones", link: "/notificaciones", imageurl: "src\assets\Images\Notificaciones.png" },
-    { title: "Mensajes", link: "/mensajes", imageurl: "src\assets\Images\Mensajes.png" }
+    { title: "Inicio", link: "/", imageurl: "Images/Home-removebg-preview.png"},
+    { title: "Notificaciones", link: "/notificaciones", imageurl: "Images/Notificaciones.png" },
+    { title: "Mensajes", link: "/mensajes", imageurl: "Images/Mensajes.png" }
   ];
 </script>
 
 
 
-<html lang="es">
-  <nav>
-    <ul>
-      {#each items as item}
-        <li>
-          <a href={item.link}><img src="..\src\assets\Images\Notificaciones.png" alt={item.title}/></a>
-        </li>
-      {/each}
-    </ul>
-  </nav>
+<nav>
+  <ul>
+    {#each items as item}
+      <li>
+        <a href={item.link} id={item.title}><img src={item.imageurl} alt={item.title}/></a>
+      </li>
+    {/each}
+  </ul>
+</nav>
+<div class = "Contenedor">
+  <div class="Contenido1">
 
-  <div class="ContenedorRaiz">
-	<main>
-		<h1>{person.name}</h1>
-		<p>Edad: {person.age}</p>
-		<p>Ubicación: {person.location}</p>
-		<p>Hobbies: {person.hobbies.join(', ')}</p>
-		<p>Biografía: {person.bio}</p>
-	  </main>
   </div>
-</html>
+  <div class="Contenido2">
+    <p>
+      <a href="1" id = "Updelimg">Upload Image</a>
+      <img src="Images/Necesario.png" alt="Necessary" id="Necess">
+    </p>
+    <br>
+    <p><a href="2" id = "Updelimg">Delete Image</a></p>
+  </div>
+  <div class="Contenido3">
+    
+  </div>
+</div>
+
 
 
 
 <style>
   img{
-    size: 200px;
+    width: 30%;
+    height: 30%;
+    filter: drop-shadow(2px 4px 2px rgba(0, 0, 0, 0.25));
   }
 
   nav {
-    background-color: #884c4c;
-    color: #fff;
+    background-color: #8D5B5B;
+    border: black;
+    border-bottom-style: solid;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 0%;
-	width: 100%;
+	  width: 100%;
   }
 
   ul {
-	list-style: none;
+	  list-style: none;
     border-radius: 10px;
     border-color: black;
     margin: 3;
@@ -63,35 +68,69 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
-	width: 100%;
+	  width: 100%;
   }
 
   li {
-    margin: 0 50px;
-	padding-left: 5%;
-	padding-right: 5%;
+
+    margin: 0 10px;
+	  padding-left: 2%;
+	  padding-right: 2%;
   }
 
-  a {
+  #Inicio {
+    display: flex;
+    justify-content: start;
     color: #fff;
     text-decoration: none;
   }
-  main {
-    background-color: #884c4c;
-    width: 90%;
-	height: 40%;
-    margin: 0 auto;
-    padding: 2rem;
-    border: 1px solid #ccc;
-    border-radius: 5px;
+  #Mensajes {
+    display: flex;
+    justify-content: end;
+    color: #fff;
+    text-decoration: none;
+  }
+  #Notificaciones {
+    display: flex;
+    justify-content: center;
+    color: #fff;
+    text-decoration: none;
   }
 
-  h1 {
-    font-size: 2rem;
-    margin-bottom: 0.5rem;
+  .Contenedor {
+    background-color: #8D5B5B;
+	  height: 100%;
+    padding: 6%;
   }
 
-  p {
-    margin-bottom: 0.5rem;
+  .Contenido1 {
+    background-color: #aa8989;
+	  height: 25%;
+    
+  }
+  .Contenido2 {
+    display: contents;
+    background-color: #cf4949;
+	  height: 10%;
+    padding-top: 1%;
+  }
+  .Contenido3 {
+    background-color: #332929;
+	  height: 13%;
+  }
+
+  #Updelimg{
+    font-style: normal;
+    font-weight: 350;
+    font-size: 200%;
+    line-height: 15px;
+    text-decoration-line: underline;
+    color: #FFFFFF;
+  }
+
+  #Necess{
+    
+    width: 2.5%;
+    height: 2.5%;
   }
 </style>
