01bde77c93fc458d73add44ab2ba3beada9ab64d
Daniel207eae1 <elbotcrack@gmail.com>
2023-02-26 14:11:27 -0500

    Profile3

--- my-svelte-app/src/App.svelte
+++ my-svelte-app/src/App.svelte
@@ -1,15 +1,15 @@
 <script>
   let person = {
-    name: 'Juan',
+    name: 'Juan Alberto Gutierrez',
     age: 28,
     location: 'Buenos Aires',
     hobbies: ['Cocinar', 'Leer', 'Viajar'],
-    bio: 'Hola xd'
+    descripcion: 'Hola me llamo juan y en realidad no existo pero es un buen test para la pagina :D'
   };
   let items = [
     { title: "Inicio", link: "/", imageurl: "Images/Home-removebg-preview.png"},
     { title: "Notificaciones", link: "/notificaciones", imageurl: "Images/Notificaciones.png" },
-    { title: "Mensajes", link: "/mensajes", imageurl: "Images/Mensajes.png" }
+    { title: "Mensajes", link: "/mensajes", imageurl: "Images/Mensajes1.png" }
   ];
 </script>
 
@@ -26,7 +26,18 @@
 </nav>
 <div class = "Contenedor">
   <div class="Contenido1">
-
+    <img src="Images/Profile.png" alt="Profile" id="Prof">
+    <div id="Datos">
+      <p>
+        {person.name}
+      </p>
+      <p>
+        {person.age}
+      </p>
+    </div>
+    <p id="Descripcion">
+      {person.descripcion}
+    </p>
   </div>
   <div class="Contenido2">
     <p>
@@ -88,7 +99,6 @@
     display: flex;
     justify-content: end;
     color: #fff;
-    text-decoration: none;
   }
   #Notificaciones {
     display: flex;
@@ -100,12 +110,14 @@
   .Contenedor {
     background-color: #8D5B5B;
 	  height: 100%;
-    padding: 6%;
+    padding: 4%;
   }
 
   .Contenido1 {
-    background-color: #aa8989;
-	  height: 25%;
+    display: flex;
+    justify-content: flex-start;
+	  height: 27%;
+    margin-bottom: 2%;
     
   }
   .Contenido2 {
@@ -129,8 +141,50 @@
   }
 
   #Necess{
-    
     width: 2.5%;
     height: 2.5%;
   }
+
+  #Prof{
+    width: 18%;
+    height: 100%;
+    box-shadow: 1px 10px 8px rgba(0, 0, 0, 0.25);
+  }
+
+  #Datos{
+    padding-left: 3%;
+    padding-right: 2%;
+    font-style: normal;
+    font-weight: 350;
+    font-size: 200%;
+    line-height: 15px;
+    color: #FFFFFF;
+    width: 40%;
+  }
+
+  #Descripcion{
+    width: 100%;
+    height: 85%;
+    padding: 2%;
+    float:right;
+    margin: 0%;
+    margin-left: 10%;
+    margin-right: 2%;
+    background-color: #62333349;
+    box-shadow: inset 6px 10px 20px rgba(0, 0, 0, 0.25);
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 200%;
+    color: #ffffff85;
+  }
+
+  #Datos > p{
+    width: 100%;
+    margin: 0%;
+    margin-top: 1%;
+    margin-bottom: 10%;
+    line-height: 100%;
+  }
+
 </style>
