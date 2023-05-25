f1a7538a3b363458496de010fb2b34047a47c6e2
AndresFelipeSaenz241218 <elbotcrack@gmail.com>
2023-05-25 15:57:58 -0500

    frntantes

--- my-svelte-app/src/firebase.js
+++ my-svelte-app/src/firebase.js
@@ -1,36 +1,35 @@
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider } from "firebase/auth";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import {getFirestore} from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDQyvLtXYpRaR8TXjLlySbheiLlf2FsCG4",
-    authDomain: "nonphotogenictinder-7d03b.firebaseapp.com",
-    projectId: "nonphotogenictinder-7d03b",
-    storageBucket: "nonphotogenictinder-7d03b.appspot.com",
-    messagingSenderId: "711996948477",
-    appId: "1:711996948477:web:76117b49a7afc0d1152394"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  export const db = getFirestore(app)
+  apiKey: "AIzaSyDQyvLtXYpRaR8TXjLlySbheiLlf2FsCG4",
+  authDomain: "nonphotogenictinder-7d03b.firebaseapp.com",
+  projectId: "nonphotogenictinder-7d03b",
+  storageBucket: "nonphotogenictinder-7d03b.appspot.com",
+  messagingSenderId: "711996948477",
+  appId: "1:711996948477:web:76117b49a7afc0d1152394",
+};
 
-  const auth = getAuth(app);
-  const provider = new GoogleAuthProvider();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
-  auth.getCurrentUser = () => {
-    return new Promise((resolve, reject) => {
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-               resolve(user)
-                const uid = user.uid;
-                localStorage.setItem('UserId',uid);
-            } else {
-              reject
-            }
-          });
-    })
-  }
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-  export {auth,provider}
+auth.getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        resolve(user);
+        //const uid = user.uid;
+      } else {
+        reject;
+      }
+    });
+  });
+};
+
+export { auth, provider };
